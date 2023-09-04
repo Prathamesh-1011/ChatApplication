9d102e61d29b94456651ef6b20e56b512427d04a
Prathamesh-1011 <144042848+Prathamesh-1011@users.noreply.github.com>
2023-09-04 21:26:37 +0530

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.chatapp;
 
-/**
- *
- * @author Pranay Rokade
- */
 import java.io.*;
 import java.net.*;
 
